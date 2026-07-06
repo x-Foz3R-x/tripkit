20f728d6730e650068b49d611b14ddb8876590b5
Foz3R <pawel.knot.1@gmail.com>
2026-07-06 23:24:10 +0200

    feat(scoreboard): add DrawerSwipeHandle component to ScoreboardPage

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -8,6 +8,7 @@ import { env } from "~/env";
 import { Skeleton } from "~/components/ui/skeleton";
 import { TeamsChart } from "~/components/modules/scoreboard/teams-chart";
 import type { Database } from "~/types/database";
+import { DrawerSwipeHandle } from "~/components/quick-drawer";
 
 type Team = Database["public"]["Tables"]["teams"]["Row"];
 
@@ -46,6 +47,7 @@ export default function ScoreboardPage() {
           <h1 className="font-heading text-theme-text text-5xl font-semibold tracking-wide drop-shadow-sm">
             Scoreboard
           </h1>
+          <DrawerSwipeHandle />
         </div>
       </header>
 
